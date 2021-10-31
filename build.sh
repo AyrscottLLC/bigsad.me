c8781c76bba9c87db7137fdd4fbdc11116578622
Jared De Blander <jared0x90@gmail.com>
2021-10-31 18:56:53 +0000

    Routine update

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/root/bigsad.me/make-reports.sh
-/root/bigsad.me/make-static.sh
-cd /root/bigsad.me
-git add .
-git commit -m "Routine update"
-git push
--- make-static.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm /root/bigsad.me/*.html
-cd /root/bigsad.me/
-wget -mpEk http://meta.ayrscott.com
-cd meta.ayrscott.com
-mv *.* ../
-rm -r /root/bigsad.me/meta.ayrscott.com/
-
